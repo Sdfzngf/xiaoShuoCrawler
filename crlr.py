7b9592bd9516a6bf2fe2f19a3804505f8356bd4d
神都服主拿高分 <65139929+Sdfzngf@users.noreply.github.com>
2024-02-14 20:09:15 +0800

    Update crlr.py

--- crlr.py
+++ crlr.py
@@ -1,15 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
 from lxml import etree
-firstz=input('请输入第一章的url:')
-fipth=input('下载目录:')
+import sys
+import getopt
+
+wqd=False
+firstz=''
+fipth=''
 ct=0
+
+def 
+
 def cnm(url):
+
 	res=requests.get(url)
 	content = BeautifulSoup(res.text, "html.parser")
 	html=etree.HTML(res.text)
 	zjmc=html.xpath('/html/body/div[1]/div[4]/div/div/div[2]/h1/text()')
-	fi=open(fipth+'/'+zjmc[0]+'-'+str(ct)+'.txt','a+')
+	fi=open(fipth+'/'+str(ct)+'_'+zjmc[0]+'.txt','a+')
 	print(zjmc)
 	xsnr=content.find_all('p',attrs={'class':'content_detail'})
 	fi.write(zjmc[0])
@@ -22,12 +30,37 @@ def cnm(url):
 	fi.close()
 	return [a,zjmc[0]]
 
-next=cnm(firstz)
-print('\"'+next[1]+'\"已爬取完毕'+'enter爬取下一章')
-input()
-ct+=1
-while True:
-	next=cnm(next[0])
+def stt():
+	global firstz,fipth,wqd
+	if not wqd:
+		firstz=input('请输入第一章的url:')
+		fipth=input('下载目录:')
+	next=cnm(firstz)
 	print('\"'+next[1]+'\"已爬取完毕'+'enter爬取下一章')
 	input()
 	ct+=1
+	while True:
+		next=cnm(next[0])
+		print('\"'+next[1]+'\"已爬取完毕'+'enter爬取下一章')
+		input()
+		ct+=1
+
+if __name__=="__main__":
+	#print("main")
+	try:
+		opts,args=getopt.getopt(sys.argv[1:],'u:p:')
+	#	print('try')
+	except getopt.GetoptError:
+	#	print('except')
+		stt()
+		sys.exit(0)
+	if len(sys.argv) > 1:
+		wqd=True
+	#	print('wqd',wqd)
+		for opt,arg in opts:
+			if opt in ['-u']:
+				firstz=arg
+			elif opt in ['-p']:
+				fipth=arg
+	stt()
+	sys.exit(0)
